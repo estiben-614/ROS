8830275f51dc7578298f7c7f9585df45136c44f8
Estiben Fernandez Echeverri <efernandeze@unal.edu.co>
2023-10-05 23:08:30 -0500

    Código para  Ejecutar roscore y turtlesim_node en paralelo

---
+++ Terminal/Paralelo.py
@@ -0,0 +1,15 @@
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+def ejecutar_comando(comando):
+    try:
+        subprocess.run(comando, check=True, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar '{comando}': {e}")
+# Abre Roscore y simulación de turtlesim_node en paralelo
+comandos = ["roscore", "rosrun turtlesim turtlesim_node"]
+
+with ThreadPoolExecutor() as executor:
+    resultados = list(executor.map(ejecutar_comando, comandos))
+
+# Los comandos se ejecutarán en paralelo
